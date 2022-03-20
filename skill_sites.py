907586cf2b8120a20dca53f3c14f7c66e2ab2f0a
AyfDROID <sergeyzargatifg@gmail.com>
2022-03-20 17:16:44 +0300

    commit

--- skill_sites.py
+++ skill_sites.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 cities = {
-    'москва': ['1540737/daa6e420d33102bf6947', '213044/7df73ae4cc715175059e'],
-    'нью-йорк': ['1652229/728d5c86707054d4745f', '1030494/aca7ed7acefde2606bdc'],
-    'париж': ["1652229/f77136c2364eb90a3ea8", '123494/aca7ed7acefd12e606bdc']
+    'москва': ['997614/56ad30efbc03beed9291', '1533899/f079b042fa8e49586e3d'],
+    'нью-йорк': ['1533899/33d7d09de5e0ade4b1a4', '213044/d09dfd821c8924149fee'],
+    'париж': ['1030494/25b8adb1ceca2ea236b3', '1521359/5c5d4ee1417a38f22759']
 }
 
 sessionStorage = {}
