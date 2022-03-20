5cd2c74ac940a309af580b8a1a9df384e651879a
AyfDROID <sergeyzargatifg@gmail.com>
2022-03-20 12:52:29 +0300

    commit

--- skill_sites.py
+++ skill_sites.py
@@ -1,4 +1,5 @@
 import os
+import random
 from flask import Flask, request
 import logging
 import json
@@ -7,6 +8,15 @@ from geo import get_country, get_distance, get_coordinates
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+cities_id = {
+    'москва': ['1540737/daa6e420d33102bf6947',
+               '213044/7df73ae4cc715175059e'],
+    'нью-йорк': ['1652229/728d5c86707054d4745f',
+                 '1030494/aca7ed7acefde2606bdc'],
+    'париж': ["1652229/f77136c2364eb90a3ea8",
+              '3450494/aca7ed7acefde22341bdc']
+}
 
 
 @app.route('/')
@@ -39,8 +49,12 @@ def handle_dialog(res, req):
     if not cities:
         res['response']['text'] = 'Ты не написал название не одного города!'
     elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - ' + \
-                                  get_country(cities[0])
+        if cities[0] in ['москва', 'нью-йорк', 'париж']:
+            res['response']['card'] = {}
+            res['response']['card']['type'] = 'BigImage'
+            res['response']['card']['title'] = 'Этот город в стране - ' + get_country(cities[0])
+            res['response']['card']['image_id'] = random.choice(cities_id[cities[0]])
+
     elif len(cities) == 2:
         distance = get_distance(get_coordinates(
             cities[0]), get_coordinates(cities[1]))
