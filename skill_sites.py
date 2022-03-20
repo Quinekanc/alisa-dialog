4d7f8b930df962338c012fa05cb12c24b31ebc3d
AyfDROID <sergeyzargatifg@gmail.com>
2022-03-20 17:12:21 +0300

    commit

--- skill_sites.py
+++ skill_sites.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 import logging
 import json
@@ -171,4 +173,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
